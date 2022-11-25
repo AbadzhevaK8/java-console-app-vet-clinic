c3511637690d09b3e220a2c770c1bb66a249f4ff
AbadzhevaK8 <make.me.loco@gmail.com>
2022-11-25 16:33:05 +0300

    feat: add basic commands.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+// logo and manual
+
+        final String LOGO = """
+                                                                                          \s
+                                                       ,,    ,,                ,,         \s
+                `7MMF'   `7MF'       mm              `7MM    db                db         \s
+                  `MA     ,V         MM                MM                                 \s
+                   VM:   ,V .gP"Ya mmMMmm      ,p6"bo  MM  `7MM  `7MMpMMMb.  `7MM  ,p6"bo \s
+                    MM.  M',M'   Yb  MM       6M'  OO  MM    MM    MM    MM    MM 6M'  OO \s
+                    `MM A' 8M""\"""\"  MM       8M       MM    MM    MM    MM    MM 8M      \s
+                     :MM;  YM.    ,  MM       YM.    , MM    MM    MM    MM    MM YM.    ,\s
+                      VF    `Mbmmd'  `Mbmo     YMbmd'.JMML..JMML..JMML  JMML..JMML.YMbmd' \s
+                """;
+        final String MANUAL = """
+                +--------------------------------------------------------+
+                |                        COMMANDS                        |
+                +--------------------------------------------------------+
+                | ADD - add a pet.                                       |
+                | LIST - data on all available pets.                     |
+                | REMOVE n - remove pet number n from the list.          |
+                | EDIT n - edit pet information.                         |
+                | EXIT - exit from the program.                          |
+                +--------------------------------------------------------+""".indent(7);
+
+        System.out.println(LOGO);
+        System.out.println(MANUAL);
+
+// data
+
         enum Gender {
             FEMALE,
             MALE
@@ -21,32 +50,7 @@ public class Main {
 
         ArrayList<Animal> pets = new ArrayList<>();
 
-        final String LOGO = """
-
-                                                                                          \s
-                                                       ,,    ,,                ,,         \s
-                `7MMF'   `7MF'       mm              `7MM    db                db         \s
-                  `MA     ,V         MM                MM                                 \s
-                   VM:   ,V .gP"Ya mmMMmm      ,p6"bo  MM  `7MM  `7MMpMMMb.  `7MM  ,p6"bo \s
-                    MM.  M',M'   Yb  MM       6M'  OO  MM    MM    MM    MM    MM 6M'  OO \s
-                    `MM A' 8M""\"""\"  MM       8M       MM    MM    MM    MM    MM 8M      \s
-                     :MM;  YM.    ,  MM       YM.    , MM    MM    MM    MM    MM YM.    ,\s
-                      VF    `Mbmmd'  `Mbmo     YMbmd'.JMML..JMML..JMML  JMML..JMML.YMbmd' \s
-                                                                                          \s
-                """;
-        final String MANUAL = """
-                +--------------------------------------------------------+
-                |                        COMMANDS                        |
-                +--------------------------------------------------------+
-                | ADD - add a pet.                                       |
-                | LIST - data on all available pets.                     |
-                | REMOVE n - remove pet number n from the list.          |
-                | EDIT n - edit pet information.                         |
-                | EXIT - exit from the program.                          |
-                +--------------------------------------------------------+""".indent(7);
-
-        System.out.println(LOGO);
-        System.out.println(MANUAL);
+// interactions
 
         String inputCommand;
         Scanner sc = new Scanner(System.in);
@@ -55,7 +59,18 @@ public class Main {
         while (!inputCommand.equalsIgnoreCase("EXIT")) {
             System.out.println("we are inside while");
             inputCommand = sc.nextLine();
-
+            inputCommand = inputCommand.toUpperCase();
+            if (inputCommand.equals("ADD")){
+                System.out.println("we are inside ADD");
+            } else if (inputCommand.equals("LIST")) {
+                System.out.println("we are inside LIST");
+            } else if (inputCommand.startsWith("REMOVE")) { // startswith
+                System.out.println("we are inside REMOVE");
+            } else if (inputCommand.startsWith("EDIT")) { // startswith
+                System.out.println("we are inside EDIT");
+            } else {
+                System.out.println("Please, enter the command.");
+            }
         }
 
     }
